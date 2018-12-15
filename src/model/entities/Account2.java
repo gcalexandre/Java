3630c5503c0cc69fdacd339aadc62dee00eba432
Alexandre Castilho <gcalexandre.castilho@gmail.com>
2018-12-15 17:06:34 -0200

    model.ProgramAccount

---
+++ src/model/entities/Account2.java
@@ -0,0 +1,70 @@
+package model.entities;
+
+import model.exceptions.DomainExceptionAccount;
+
+public class Account2 {
+	
+	private int number;
+	private String holder;
+	private double balance;
+	private double withdrawLimit;
+	
+	
+	public Account2() {
+		
+	}
+	
+	public Account2(int number, String holder, double balance, double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	
+	public double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		if (amount > withdrawLimit) {
+			throw new DomainExceptionAccount("The amount exceeds withdraw limit");
+		}
+		if (amount > balance) {
+			throw new DomainExceptionAccount("Not enough balance");
+		}
+		
+		balance -= amount;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/model/exceptions/DomainExceptionAccount.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class DomainExceptionAccount extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DomainExceptionAccount(String msg) {
+		super(msg);
+	}
+
+}
